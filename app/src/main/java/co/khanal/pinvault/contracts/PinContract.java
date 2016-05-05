de063709724083992458440bd5c60d5ca3f9a80b
Abhi Khanal <khanal.abhi@gmail.com>
2016-05-05 18:07:09 -0400

    Added contracts for the database.

---
+++ app/src/main/java/co/khanal/pinvault/contracts/PinContract.java
@@ -0,0 +1,14 @@
+package co.khanal.pinvault.contracts;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by abhi on 5/5/16.
+ */
+public class PinContract {
+
+    public static String DATABASE_NAME = "pinvault";
+
+    private SQLiteDatabase db;
+
+}
