5fac583a4eec1681c2436e42181f6247fb72fa66
Abhi Khanal <khanal.abhi@gmail.com>
2016-05-05 18:12:40 -0400

    Cleaned up the contract.

--- app/src/main/java/co/khanal/pinvault/contracts/PinContract.java
+++ app/src/main/java/co/khanal/pinvault/contracts/PinContract.java
@@ -1,14 +1,24 @@
 package co.khanal.pinvault.contracts;
 
-import android.database.sqlite.SQLiteDatabase;
-
 /**
  * Created by abhi on 5/5/16.
  */
 public class PinContract {
 
     public static String DATABASE_NAME = "pinvault";
+    public static String TABLE_NAME = "pinvault";
+    public static String ID_COLUMN = "_id";
+    public static String LABEL_COLUMN = "label";
+    public static String PIN_COLUMN = "pin";
+    public static String NOTES_COLUMN = "notes";
 
-    private SQLiteDatabase db;
+    public static String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( " +
+            ID_COLUMN + " INTEGER PRIMARY AUTOINCREMENT," +
+            LABEL_COLUMN + " TEXT," +
+            PIN_COLUMN + " TEXT," +
+            NOTES_COLUMN + " TEXT " +
+            ");";
+
+    public static String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
 
 }
