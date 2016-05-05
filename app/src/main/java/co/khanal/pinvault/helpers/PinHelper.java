e90c2e50d74c1e5adc26fe598cd282c7f542823d
Abhi Khanal <khanal.abhi@gmail.com>
2016-05-05 18:27:40 -0400

    Cleaned up the helpers.

--- app/src/main/java/co/khanal/pinvault/helpers/PinHelper.java
+++ app/src/main/java/co/khanal/pinvault/helpers/PinHelper.java
@@ -1,10 +1,15 @@
 package co.khanal.pinvault.helpers;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.sql.SQLException;
+
 import co.khanal.pinvault.contracts.PinContract;
+import co.khanal.pinvault.pojos.Pin;
 
 /**
  * Created by abhi on 5/5/16.
@@ -23,5 +28,23 @@ public class PinHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(PinContract.DROP_TABLE);
         db.execSQL(PinContract.CREATE_TABLE);
+    }
+
+    public void insertPin(Pin pin) throws SQLException {
+        ContentValues values = new ContentValues();
+        values.put(PinContract.LABEL_COLUMN, pin.getLabel());
+        values.put(PinContract.PIN_COLUMN, pin.getPin());
+        values.put(PinContract.NOTES_COLUMN, pin.getNotes());
+
+        SQLiteDatabase db = getWritableDatabase();
+        db.insertOrThrow(PinContract.TABLE_NAME,
+                null,
+                values);
+    }
+
+    public void removePin(int _id) throws SQLException{
+        SQLiteDatabase db = getWritableDatabase();
+        Integer[] boundArgs = {_id};
+        db.execSQL("DELETE * FROM " + PinContract.TABLE_NAME + " WHERE " + PinContract.ID_COLUMN + "=?;", boundArgs);
     }
 }
