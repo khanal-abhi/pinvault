cb329e7de85d5b7f307c3a59f911768f4f5d9655
Abhi Khanal <khanal.abhi@gmail.com>
2016-05-17 20:44:26 -0400

    Added test file.

---
+++ app/src/androidTest/java/co/khanal/pinvault/PinTest.java
@@ -0,0 +1,25 @@
+package co.khanal.pinvault;
+
+import android.content.Context;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by abhi on 5/17/16.
+ */
+public class PinTest extends TestCase {
+
+    Context mContext;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+}
